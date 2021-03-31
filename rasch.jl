05b73e8bf0917ffd8c474754a9d54a3e827c710a
Tobias Alfers <mail@t-alfers.com>
2021-03-31 12:57:59 +0200

    six times faster sampling

--- rasch.jl
+++ rasch.jl
@@ -1,6 +1,6 @@
 # Import Packages
 using DelimitedFiles, DataFrames;
-using ReverseDiff, Turing;
+using ReverseDiff, Turing, Memoization, LazyArrays;
 using StatsFuns: logistic;
 ## using BenchmarkTools;
 
@@ -8,6 +8,8 @@ using StatsFuns: logistic;
 function intArray(a::CategoricalArray)
     map(x -> Int(x), CategoricalArrays.order(a.pool)[a.refs]);
 end
+lazyarray(f, x) = LazyArray(Base.broadcasted(f, x))
+safelogistic(x::T) where {T} = logistic(x) * (1 - 2 * eps(T)) + eps(T)
 
 # data handling stuff
 verbal = readdlm("./dichotom.txt", '\t', Int, '\n'; header = true);
@@ -16,27 +18,27 @@ dfVerbal = DataFrame(verbal[1]);
 rename!(dfVerbal,  Symbol.(permutedims(verbal[2])[:]));
 dfVerbal.id = 1:nrow(dfVerbal);
 
-verbal_responses = stack(dfVerbal[:, Not([:Anger, :Sex])], Not(:id), :id);
+verbal_responses = DataFrames.stack(dfVerbal[:, Not([:Anger, :Sex])], Not(:id), :id);
 verbal_responses.variable = intArray(verbal_responses.variable);
 
 # statistical modeling
-@model irt_1pl(y, ii, jj) = begin
-    I = maximum(ii);
-    J = maximum(jj);
+@model rasch(y, ii, jj) = begin
+  I = maximum(ii);
+  J = maximum(jj);
 
-    # prior item parameter
-    itempar_σ ~ truncated(Cauchy(0, 5), 0, Inf);
-    itempar ~ filldist(Normal(0, itempar_σ), I)
+  # prior item parameter
+  itempar_σ ~ truncated(Cauchy(0, 5), 0, Inf);
+  itempar ~ filldist(Normal(0, itempar_σ), I)
 
-    # prior person parameter
-    perspar_σ ~ truncated(Cauchy(0, 5), 0, Inf);
-    perspar ~ filldist(Normal(0, perspar_σ), J);
+  # prior person parameter
+  perspar_σ ~ truncated(Cauchy(0, 5), 0, Inf);
+  perspar ~ filldist(Normal(0, perspar_σ), J);
 
-    # target distribution
-    η = logistic.(perspar[jj] - itempar[ii])
-    y .~ Bernoulli.(η)
+  # target distribution
+  y ~ arraydist(lazyarray(x -> Bernoulli(safelogistic(x)), perspar[jj] .- itempar[ii]))
 end
 
 Turing.setadbackend(:reversediff)
-chn = sample(irt_1pl(verbal_responses.value, verbal_responses.variable, verbal_responses.id), NUTS(1000, 0.8), 2000, progress = true)
-# NUTS(1000, 0.8), 2000 :: 276.295 s (5937990687 allocations: 294.14 GiB)
+Turing.setrdcache(true)
+chn = sample(rasch(verbal_responses.value, verbal_responses.variable, verbal_responses.id), NUTS(1000, 0.8), 2000, progress = false)
+# NUTS(1000, 0.8), 2000 :: 43.783335 seconds (15.58 M allocations: 3.886 GiB, 0.78% gc time
