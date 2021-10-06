47e7468185633d8db1afaf661ceb1f5182b19bb8
Tobias Alfers <8092306+t-alfers@users.noreply.github.com>
2021-10-06 09:57:27 +0200

    Update rasch.jl

--- rasch.jl
+++ rasch.jl
@@ -1,43 +1,50 @@
 # Import Packages
-using DelimitedFiles, DataFrames;
-using ReverseDiff, Turing, Memoization, LazyArrays;
-using StatsFuns: logistic;
-## using BenchmarkTools;
+using Random, Distributions, Turing, Memoization, LazyArrays, ReverseDiff
+using CSV, DataFrames, CategoricalArrays
 
 # helper functions
-intArray(a::CategoricalArray) = map(x -> Int(x), CategoricalArrays.order(a.pool)[a.refs]);
 lazyarray(f, x) = LazyArray(Base.broadcasted(f, x))
-safelogistic(x::T) where {T} = logistic(x) * (1 - 2 * eps(T)) + eps(T)
 
-# data handling stuff
-verbal, headerVerbal = readdlm("./dichotom.txt", '\t', Int, '\n'; header = true);
+# Settings for Turing
+Random.seed!(1234)
+Turing.setrdcache(true)
+Turing.setadbackend(:reversediff)
 
-dfVerbal = DataFrame(verbal);
-rename!(dfVerbal,  Symbol.(permutedims(headerVerbal)[:]));
-dfVerbal.id = 1:nrow(dfVerbal);
+# Data Handling stuff
+url = "https://raw.githubusercontent.com/t-alfers/Rasch-Turing/master/dichotom.txt"
+data = CSV.read(download(url), DataFrame, delim = "\t")
+data.person = collect(1:nrow(data));
+data_responses = data[:, Not([:Anger, :Sex])];
 
-verbal_responses = DataFrames.stack(dfVerbal[:, Not([:Anger, :Sex])], Not(:id), :id);
-verbal_responses.variable = intArray(verbal_responses.variable);
+data_long = DataFrames.stack(data_responses, Not(:person), :person);
+data_long.variable = CategoricalArray(data_long.variable)
+levels!(data_long.variable, unique(data_long.variable))
+data_long.variable = levelcode.(data_long.variable)
 
 # statistical modeling
 @model rasch(y, ii, jj) = begin
   I = maximum(ii);
   J = maximum(jj);
+  N = I * J;
 
   # prior item parameter
-  itempar_σ ~ truncated(Cauchy(0, 5), 0, Inf);
-  itempar ~ filldist(Normal(0, itempar_σ), I)
+  β_σ ~ truncated(Cauchy(0, 5), 0, Inf);
+  β ~ filldist(Normal(0, β_σ), I);
 
   # prior person parameter
-  perspar_σ ~ truncated(Cauchy(0, 5), 0, Inf);
-  perspar ~ filldist(Normal(0, perspar_σ), J);
+  θ_σ ~ truncated(Cauchy(0, 5), 0, Inf);
+  θ ~ filldist(Normal(0, θ_σ), J);
 
   # target distribution
-  y ~ arraydist(lazyarray(x -> Bernoulli(safelogistic(x)), perspar[jj] .- itempar[ii]))
+  y ~ arraydist(lazyarray(x -> BernoulliLogit(x), θ[jj] .- β[ii]))
 end
 
-Turing.setadbackend(:reversediff)
-Turing.setrdcache(true)
-chn = sample(rasch(verbal_responses.value, verbal_responses.variable, verbal_responses.id),
-             NUTS(1000, 0.8), 2000, progress = false)
+@time chn = sample(rasch(data_long.value, data_long.variable, data_long.person),
+             NUTS(1000, 0.8), 2000, progress = true)
+
+# LazyArrays 0.16.16
 # NUTS(1000, 0.8), 2000 :: 43.783335 seconds (15.58 M allocations: 3.886 GiB, 0.78% gc time)
+#
+# LaryArrays 0.22.2
+# NUTS(1000, 0.8), 2000 :: 354.621508 seconds (16.82 M allocations: 3.857 GiB, 0.09% gc time)
+#
